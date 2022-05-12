7977854d58f935db50bd1f8f38f60fec663922d4
Tom <tomektomeknyc@yahoo.com>
2022-05-12 22:52:34 +0200

    New file

--- src/com/company/Shell_Sort.java
+++ src/com/company/Shell_Sort.java
@@ -11,7 +11,7 @@ int array = intArray.length;
 System.out.print("The array length is: " + array);
 System.out.println("");
         for(int gap = intArray.length/2; gap >0; gap /=2){
-
+System.out.println("Gap: " + gap);
             for(int i = gap; i < intArray.length; i++){
                 int newElement = intArray[i];
                 int j = i;
