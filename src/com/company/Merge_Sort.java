6f041c855819d9c676491442baf6b2a7e6862946
Tom <tomektomeknyc@yahoo.com>
2022-05-14 17:13:39 +0200

    New file

---
+++ src/com/company/Merge_Sort.java
@@ -0,0 +1,43 @@
+package com.company;
+
+/**
+ * Created by Tom on 5/13/2022.
+ */
+public class  Merge_Sort {
+
+    public static void main (String args[]){
+        int [] intArray = {20, 35, -15, 7, 55, 1, -22};
+ mergeSort(intArray, 0, intArray.length);
+
+        for (int i = 0; i <  intArray.length; i++){
+            System.out.print(intArray[i]);
+            System.out.print(" ");
+
+        }
+    }
+// int [] intArray = {20, 35, -15, 7, 55, 1, -22};
+public static void mergeSort(int[] input, int start, int end){
+        if(end - start < 2){
+            return;
+        }
+        int mid = (start + end)/2;
+        mergeSort(input, start, mid);
+        mergeSort(input,mid,end);
+        merge(input, start, mid,end);
+}
+public static void merge(int[] input, int start, int mid, int end){
+    if (input[mid - 1]<=  input[mid]){
+        return;
+    }
+    int i = start;
+    int j = mid;
+    int tempIndex = 0;
+    int [] temp = new int[end - start ];
+    while (i < mid && j < end) {
+
+        temp[tempIndex++]= input[i] <= input[j] ? input[i++] : input[j++];
+    }
+     System.arraycopy(input, i, input, start + tempIndex, mid -i);
+    System.arraycopy(temp, 0, input, start, tempIndex);
+}
+}
